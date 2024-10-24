61a36d8789695563a7f52015151980494b082f81
Mateus Ferreira <mateusanjos225@gmail.com>
2024-10-24 14:21:38 -0300

    Pequenas alterações em cores e temas do app

--- app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
+++ app/src/main/java/co/tiagoaguiar/ganheinamega/MainActivity.kt
@@ -1,13 +1,11 @@
 package co.tiagoaguiar.ganheinamega
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 import java.util.Random
 
 class MainActivity : AppCompatActivity() {
@@ -19,35 +17,39 @@ class MainActivity : AppCompatActivity() {
         val txtResult: TextView = findViewById(R.id.txt_result)
         val btnGenerator: Button = findViewById(R.id.btn_generate)
 
-        btnGenerator.setOnClickListener{
+        btnGenerator.setOnClickListener {
             val text = editText.text.toString()
             numberGenerator(text, txtResult)
         }
     }
-    private fun numberGenerator(text: String, textResult: TextView){
-        if (text.isNotEmpty()){
-            val numberValidator = text.toInt()
-            if (numberValidator in 6..15){
 
-                val listNumber = mutableSetOf<Int>()
-                val random = Random()
-
-                while (true){
-                    val number = random.nextInt(60)
-                    listNumber.add(number + 1)
-
-                    if (listNumber.size == numberValidator){
-                        break
-                    }
-                }
-
-                textResult.text = listNumber.joinToString(" - ")
-
-            } else {
-                Toast.makeText(this, "Valor não permitido. Informe um número entre 6 e 15!!", Toast.LENGTH_LONG).show()
-            }
-        } else {
+    private fun numberGenerator(text: String, textResult: TextView) {
+        if (text.isEmpty()) {
             Toast.makeText(this, "Não é permitido campo vazio", Toast.LENGTH_LONG).show()
+            return
         }
+
+        val numberValidator = text.toInt()
+        if (numberValidator < 6 || numberValidator > 15) {
+            Toast.makeText(
+                this,
+                "Valor não permitido. Informe um número entre 6 e 15!!",
+                Toast.LENGTH_LONG
+            ).show()
+            return
+        }
+
+        val listNumber = mutableSetOf<Int>()
+        val random = Random()
+
+        while (true) {
+            val number = random.nextInt(60)
+            listNumber.add(number + 1)
+
+            if (listNumber.size == numberValidator) {
+                break
+            }
+        }
+        textResult.text = listNumber.joinToString(" - ")
     }
 }
